a460f50e9726acae0daca53a9e7a33424fa4936b
SSND-Nuchos <nouh.elomri@student.ssnd.sk>
2025-05-26 11:36:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 import sqlite3
 
 app = Flask(__name__)
 
 def pripoj_db():
     return sqlite3.connect("elomri-kurzy.db")
+
+def affin_sifra(text, A, B):
+    vysledok = ""
+    for znak in text.upper():
+        if znak.isalpha():
+            c = ord(znak) - ord('A')
+            sifrovane_c = (A * c + B) % 26
+            vysledok += chr(sifrovane_c + ord('A'))
+        else:
+            vysledok += znak
+    return vysledok
 
 @app.route('/')
 def index():
@@ -13,7 +24,40 @@ def index():
         <a href="/treneri"><button>Zobraz trénerov a ich kurzy</button></a>
         <a href="/kurzy"><button>Zobraz všetky kurzy</button></a>
         <a href="/miesta"><button>Zobraz všetky miesta</button></a>
-        <a href="/sucet_kapacity"><button>Súčet kapacity kurzov na 'P'</button></a>
+        <a href="/sucet_kapacity"><button>Súčet kapacity kurzov na 'P'</button></a><br><br>
+        <a href="/vloz_kurz"><button>Vlož nový kurz</button></a>
+    '''
+
+@app.route('/vloz_kurz', methods=['GET', 'POST'])
+def vloz_kurz():
+    if request.method == 'POST':
+        nazov = request.form['nazov']
+        typ = request.form['typ']
+        max_kapacita = request.form['max_kapacita']
+        id_trenera = request.form['id_trenera']
+
+        A, B = 5, 8
+        nazov_sifra = affin_sifra(nazov, A, B)
+        typ_sifra = affin_sifra(typ, A, B)
+
+        conn = pripoj_db()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Kurzy (Nazov, TypSportu, MaxKapacita, ID_Trenera) VALUES (?, ?, ?, ?)",
+                       (nazov_sifra, typ_sifra, max_kapacita, id_trenera))
+        conn.commit()
+        conn.close()
+        return '<p>Úspešne pridané!</p><a href="/">Späť na úvod</a>'
+
+    return '''
+        <h2>Vloženie kurzu</h2>
+        <form method="post">
+            Názov kurzu: <input name="nazov"><br>
+            Typ športu: <input name="typ"><br>
+            Max kapacita: <input name="max_kapacita" type="number"><br>
+            ID trénera: <input name="id_trenera" type="number"><br>
+            <input type="submit" value="Vložiť">
+        </form>
+        <a href="/">Späť</a>
     '''
 
 @app.route('/treneri')
